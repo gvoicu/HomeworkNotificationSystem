62b145174a08167b975370f75c7a3c98e59524f3
Gabriel Voicu <ping.gabi@gmail.com>
2012-04-28 14:17:10 +0300

    Created project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PoliTodo::Application.config.secret_token = '3bdb8ee661b36312ec35faa4ce3fd9d19aef7a23b113bc7cc969c164b47e524c4fe0f5e049662389c7eb95d7d2d0b7e7b2e32cb2df91a94cec211af24d2cbe2c'
