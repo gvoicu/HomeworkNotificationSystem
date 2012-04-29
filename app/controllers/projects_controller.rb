ed6107a3298043b68cd0bb756dc9372b122e855c
Andu <andu@andu-LIFEBOOK-AH530.(none)>
2012-04-29 17:37:11 +0300

    1) Changed design for the sign up page 2) Repair broken links and the index interface

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,7 +7,7 @@ class ProjectsController < ApplicationController
 
   def create
         project = Project.new()
-	      project.name = params[:name]
+	    project.name = params[:name]
        	project.where = params[:where]
         project.project_type = 1 
         project.subject_id = params[:subject_id]
@@ -25,6 +25,23 @@ class ProjectsController < ApplicationController
 	def show
     @project = Project.where(:id => params[:id]).first
 	end
+
+  def edit
+    @project = Project.where(:id => params[:id]).first()
+  end
+  
+  def update
+        project = Project.where(:id => params[:id]).first()
+	    project.name = params[:name]
+       	project.where = params[:where]
+        project.project_type = 1 
+        project.subject_id = params[:subject_id]
+        project.deadline= DateTime.parse(params[:when])
+        project.priority = params[:priority]
+        project.other_details = params[:other_details]
+        project.save
+        redirect_to "/projects"
+  end
 
   def destroy
    Project.delete_all(:id => params[:id])
