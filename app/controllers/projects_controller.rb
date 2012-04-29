d8764a227c6554b778b1dad51bcca6a3bd1de295
Gabriel Voicu <ping.gabi@gmail.com>
2012-04-29 12:05:33 +0300

    Fixed the project creation

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,13 +6,12 @@ class ProjectsController < ApplicationController
   end
 
   def create
-        raise "A intrat in create"
         project = Project.new()
 	      project.name = params[:name]
        	project.where = params[:where]
         project.project_type = 1 
         project.subject_id = params[:subject_id]
-        project.deadline= DateTime.parse(params[:deadline])
+        project.deadline= DateTime.parse(params[:when])
         project.priority = params[:priority]
         project.other_details = params[:other_details]
         project.save
